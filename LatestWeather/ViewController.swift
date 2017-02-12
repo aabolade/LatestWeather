4844e1e7fd9bfe8cbaacf4669e690662d9f977d0
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-02-12 22:57:44 +0000

    initial commit

---
+++ LatestWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LatestWeather
+//
+//  Created by Leke Abolade on 12/02/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
