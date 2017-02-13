9b4b4998e9c31c0c00453061f6fbc42fe724f30e
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-02-13 21:28:42 +0000

    tests for API service and current weather objects

--- LatestWeather/APIService.swift
+++ LatestWeather/APIService.swift
@@ -11,11 +11,24 @@ import Alamofire
 
 class APIService {
     
+    
+    
     typealias JSONDictionaryCompletion = (Forecast?) -> Void
     
     func getWeatherJSON(_ completion: @escaping JSONDictionaryCompletion) {
         
+        let clientKeys = ClientKeys()
+        let apiKey = clientKeys.valueForAPIKey(named: "API_CLIENT_ID")
         
+        Alamofire.request("https://api.darksky.net/forecast/\(apiKey)/42.3601,-71.0589").responseJSON { response in
+            
+            guard let JSON = response.result.value as? [String: AnyObject] else {
+                print("There is no data")
+                return
+            }
+            let forecast = Forecast(forecastDictionary: JSON)
+            completion(forecast)
+        }
         
     }
     
--- LatestWeatherTests/CurrentWeatherTests.swift
+++ LatestWeatherTests/CurrentWeatherTests.swift
@@ -19,10 +19,10 @@ class CurrentWeatherTests: XCTestCase {
         let weatherDictionary = ["summary": "partly cloudy", "temperature": "60", "humidity": "0.68", "precipProbability":"0.2"]
         let currentWeather = CurrentWeather(weatherDictionary: weatherDictionary as [String : AnyObject])
         
-        XCTAssertEqual(currentWeather.temperature, "60")
-        XCTAssertEqual(currentWeather.precipitation, "0.2")
+        XCTAssertEqual(currentWeather.temperature, 60)
+        XCTAssertEqual(currentWeather.precipitation, 20)
         XCTAssertEqual(currentWeather.summary, "partly cloudy")
-        XCTAssertEqual(currentWeather.humidity, "0.68")
+        XCTAssertEqual(currentWeather.humidity, 68)
     }
     
     override func tearDown() {
