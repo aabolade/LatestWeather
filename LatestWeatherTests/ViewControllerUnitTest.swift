bf113c4d05e034a054c3743768f3666b419270f8
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-02-13 01:19:08 +0000

    adds method to make a call to api service object

---
+++ LatestWeatherTests/ViewControllerUnitTest.swift
@@ -0,0 +1,43 @@
+//
+//  ViewControllerUnitTest.swift
+//  LatestWeather
+//
+//  Created by Leke Abolade on 12/02/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import XCTest
+@testable import LatestWeather
+
+class ViewControllerUnitTest: XCTestCase {
+    
+    var viewController: UIViewController!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testHasMethodThatCallsAPIService() {
+    
+        class FakeAPIService: APIService {
+            var getWeatherJSONWasCalled = false
+        
+             override func getWeatherJSON() {
+                getWeatherJSONWasCalled = true
+            }
+        }
+        
+        let vc = ViewController()
+        vc.apiService = FakeAPIService()
+        vc.getCurrentWeather()
+        
+        XCTAssertTrue((vc.apiService as! FakeAPIService).getWeatherJSONWasCalled)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+
+    
+}
